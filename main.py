f48d37ae52b428bf7a23fbab3273bd19f96453e9
Chan <zzl831129@gmail.com>
2026-07-20 02:33:13 +0800

    Remove local debug code and fix env variable loading

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import time
-from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
@@ -12,13 +11,8 @@ load_dotenv()
 # 2. 初始化 FastAPI
 app = FastAPI()
 
-# 3. 獲取 Token 並初始化 LINE API
-token = os.getenv('JKqPdMo3B9SDyR9L8sB2rFVquQVO9n5IinedCb0JMylk4w8a+yQk141EiwTWZLxwyoWN0LP+DWlyCgSEILaBzd Vp4d+n1vDCgk2W5YC8SJcuGJ9HNm6MfZ1p3I7axfcFK/HRoX4lv27DiOL5irh1NAdB04t89/1O/w1cDnyilFU=')
-if not token:
-    # 這行是為了除錯，先暫時把 Token 直接寫在這裡測試看看
-    # 如果確定要正式使用，請把這行換成 print 後 exit
-    print("錯誤：無法從 .env 讀取到 Token")
-    token = "這裡貼上你那串超長的 Token 進行測試" 
+token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+line_bot_api = LineBotApi(token)
 
 line_bot_api = LineBotApi(token)
 
